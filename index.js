250fa81841c5c2e14ab5aaae941568e5daa08daf
samarimam <samarimam78@gmail.com>
2021-09-10 23:17:00 +0530

    bug fixing1

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-import express from 'express';
-import Connection from './database/db.js';
-import Router from './routes/route.js';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-
+import express from "express";
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/", Router);
 
-app.use('/', Router);
-
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server is running in port ${PORT}`));
-
-
 
 Connection();
 // w4bCFhKWO9qVmDCO
